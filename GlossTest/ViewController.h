84e1eaffa4a4ecfcf14f330dbf6b6fa2ba7c59e5
Arnas Dundulis <arnas@vinted.com>
2016-01-27 08:59:00 +0200

    Initial Commit

---
+++ GlossTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GlossTest
+//
+//  Created by Arnas Dundulis on 1/27/16.
+//  Copyright © 2016 abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GlossTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GlossTest
+//
+//  Created by Arnas Dundulis on 1/27/16.
+//  Copyright © 2016 abc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
